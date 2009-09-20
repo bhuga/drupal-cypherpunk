feb9004b5da70f25ea68705686714d9b6c05ee85
Arto Bendiken <arto@bendiken.net>
2009-09-20 13:26:48 +0200

    Updated Organic Groups.

---
+++ profiles/cypherpunk/modules/og/og.js
@@ -0,0 +1,18 @@
+// $Id: og.js,v 1.9.2.2 2009/09/17 12:52:42 weitzman Exp $
+
+// Content type form
+Drupal.behaviors.og_content_type = function() {
+  // Disable the group limit textarea if the content type is not a standard group post
+  $('input[name="og_content_type_usage"]').click(function(){
+    if (!$('#edit-og-content-type-usage-group-post-standard').attr('checked')) {
+      $('#edit-og-max-groups').attr('disabled','disabled');
+    } else {
+      $('#edit-og-max-groups').removeAttr('disabled');
+    }
+  });
+  
+  // Initial check to see if content type is standard group post
+  if (!$('#edit-og-content-type-usage-group-post-standard').attr('checked')) {
+    $('#edit-og-max-groups').attr('disabled','disabled');
+  };
+}
